f6d727b41d81fbb2aac9a6fbd81d0b09323b6b57
Mary Renee Arguelles <mary.arguelles@mary.arguelles-CPJFF29RD7>
2022-10-05 07:07:19 +0800

    Add the lib.js file with functions - square() - fahrenheit() - rectangleArea() - isPalindrome()

---
+++ lib.js
@@ -0,0 +1,7 @@
+const square = (n) => n * n;
+
+const fahrenheit = (celsius) => celsius * 1.8 + 32;
+
+const rectangleArea = (a, b) => a * b;
+
+const isPalindrome = (str) => [...str.toUpperCase()].reverse().join('') === str.toUpperCase();
