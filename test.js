beb4f3fb43b4c5cd33311dcebed019fe498ace16
Mary Renee Arguelles <mary.arguelles@mary.arguelles-CPJFF29RD7>
2022-10-05 10:26:08 +0800

    Add test cases for multiple module export

---
+++ test.js
@@ -0,0 +1,7 @@
+const { square, fahrenheit, rectangleArea, isPalindrome } = require('./lib');
+
+console.log(square(8));
+console.log(fahrenheit(35));
+console.log(rectangleArea(9, 4));
+console.log(isPalindrome("CiVIc"));
+console.log(isPalindrome("AbAbbA"));
