f7ae29deddc40568ac6ea0c3fce4b111b17b6749
Mary Renee Arguelles <mary.arguelles@mary.arguelles-CPJFF29RD7>
2022-10-11 09:16:57 +0800

    Apply prettier to js files

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const { square, fahrenheit, rectangleArea, isPalindrome } = require('./lib');
 console.log(square(8));
 console.log(fahrenheit(35));
 console.log(rectangleArea(9, 4));
-console.log(isPalindrome("CiVIc"));
-console.log(isPalindrome("AbAbbA"));
+console.log(isPalindrome('CiVIc'));
+console.log(isPalindrome('AbAbbA'));
--- lib.js
+++ lib.js
@@ -4,11 +4,12 @@ const fahrenheit = (celsius) => celsius * 1.8 + 32;
 
 const rectangleArea = (a, b) => a * b;
 
-const isPalindrome = (str) => [...str.toUpperCase()].reverse().join('') === str.toUpperCase();
+const isPalindrome = (str) =>
+  [...str.toUpperCase()].reverse().join('') === str.toUpperCase();
 
 module.exports = {
-    square,
-    fahrenheit,
-    rectangleArea,
-    isPalindrome
-}
+  square,
+  fahrenheit,
+  rectangleArea,
+  isPalindrome,
+};
