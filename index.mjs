5fb9c8ea8b7478605e290127a8cedf0545a2c22d
Mary Renee Arguelles <mary.arguelles@mary.arguelles-CPJFF29RD7>
2022-10-26 10:57:11 +0800

    Convert cjs to esm and prettier to eslint

---
+++ index.mjs
@@ -0,0 +1,9 @@
+import {
+  square, fahrenheit, rectangleArea, isPalindrome,
+} from './lib.mjs'
+
+console.log(square(8))
+console.log(fahrenheit(35))
+console.log(rectangleArea(9, 4))
+console.log(isPalindrome('CiVIc'))
+console.log(isPalindrome('AbAbbA'))
---
+++ lib.mjs
@@ -0,0 +1,7 @@
+export const square = (n) => n * n
+
+export const fahrenheit = (celsius) => celsius * 1.8 + 32
+
+export const rectangleArea = (a, b) => a * b
+
+export const isPalindrome = (str) => [...str.toUpperCase()].reverse().join('') === str.toUpperCase()
